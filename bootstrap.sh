cc47c7ca838cf06b0167f7df463053928b576b75
nadya <nadya>
2007-09-19 11:43:37 +0000

    force order installing rpms

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@
 # @Copyright@
 #
 # $Log$
+# Revision 1.1.4.2  2007/09/19 11:43:37  nadya
+# force order installing rpms
+#
 # Revision 1.1.4.1  2007/09/19 10:14:27  nadya
 # bring from 4.2.1
 #
@@ -13,6 +16,16 @@
 #
 . ../etc/bootstrap-functions.sh
 
+compile RPMS
 install_os_packages gfarm-base
 
+compile SRPMS
+install fuse
+install fuse-devel
+install fuse-libs
+install gfarm-libs
+install gfarm-client
+install gfarm-devel
 
+compile SRPMS
+install_os_packages gfarm-base
