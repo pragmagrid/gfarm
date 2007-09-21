f218c4f3a739ca6a2d3a6a9e30453a7214480da9
nadya <nadya>
2007-09-21 08:33:05 +0000

    initial revision

--- include/javascript/gfarm.js
+++ include/javascript/gfarm.js
@@ -2,71 +2,29 @@
  * $Id$
  *
  * @Copyright@
- * 
- * 				Rocks(tm)
- * 		         www.rocksclusters.org
- * 		        version 4.3 (Mars Hill)
- * 
- * Copyright (c) 2000 - 2007 The Regents of the University of California.
- * All rights reserved.	
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- * 
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice unmodified and in its entirety, this list of conditions and the
- * following disclaimer in the documentation and/or other materials provided 
- * with the distribution.
- * 
- * 3. All advertising and press materials, printed or electronic, mentioning
- * features or use of this software must display the following acknowledgement: 
- * 
- * 	"This product includes software developed by the Rocks(tm)
- * 	Cluster Group at the San Diego Supercomputer Center at the
- * 	University of California, San Diego and its contributors."
- * 
- * 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
- * neither the name or logo of this software nor the names of its
- * authors may be used to endorse or promote products derived from this
- * software without specific prior written permission.  The name of the
- * software includes the following terms, and any derivatives thereof:
- * "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
- * the associated name, interested parties should contact Technology 
- * Transfer & Intellectual Property Services, University of California, 
- * San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
- * Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
- * 
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
  * @Copyright@
  * 
  * $Log$
- * Revision 1.2  2007/08/08 12:58:10  nadya
- * add verification function for each form field
- *
- * Revision 1.1  2007/08/07 11:48:48  nadya
+ * Revision 1.2.2.1  2007/09/21 08:33:05  nadya
  * initial revision
  *
- *
  */
+
+function check_LocalHost(hostname)
+{
+	var retval = false;
+
+	if (hostname.toLowerCase() == "localhost")
+		retval = true; 
+
+	return(retval);
+}
 
 function check_name(hostname)
 {
 	var retval = true;
+	if (check_LocalHost(hostname) == true) 
+		return(retval);
 
 	if (check_fqdn(hostname) == false) {
 		if (check_ipaddr(hostname) == false) {
@@ -84,10 +42,9 @@ function check_GFmetaserver(e)
 	/*
 	 * the user-input variable
 	 */
-	var MetaServer = doc.getElementsByName(
-					'Gfarm_MetaServer')[0];
+	var GfarmMetaServer = doc.getElementsByName('Info_GfarmMetaServer')[0];
 
-	return(check_name(MetaServer));
+	return(check_name(GfarmMetaServer));
 }
 
 function check_GFagent(e)
@@ -97,10 +54,9 @@ function check_GFagent(e)
 	/*
 	 * the user-input variable
 	 */
-	var Agent = doc.getElementsByName(
-					'Gfarm_Agent')[0];
+	var GfarmAgent = doc.getElementsByName('Info_GfarmAgent')[0];
 
-	return(check_name(Agent));
+	return(check_name(GfarmAgent));
 }
 
 function check_GFfsnode(e)
@@ -110,11 +66,10 @@ function check_GFfsnode(e)
 	/*
 	 * the user-input variable
 	 */
-	var FSNode = doc.getElementsByName(
-					'Gfarm_FSNode')[0];
-	if (FSNode == "No")
-		retrn(true)
+	var GfarmFSNode = doc.getElementsByName('Info_GfarmFSNode')[0];
+	if (GfarmFSNode.toLowerCase() == "none")
+		return(true); 
 
-	return(check_name(Agent));
+	return(check_name(GfarmFSNode));
 }
 
